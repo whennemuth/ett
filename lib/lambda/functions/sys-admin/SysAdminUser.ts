07e382c3b6b48c99c79866cb0f67ec907e6b7291
Hennemuth <wrh@bu.edu>
2025-01-13 22:33:16 -0500

    BUGFIX: "retract-invitation" call needs to be accounted for in the sysadmin role api backend

--- lib/lambda/functions/sys-admin/SysAdminUser.ts
+++ lib/lambda/functions/sys-admin/SysAdminUser.ts
@@ -10,7 +10,7 @@ import { EntityToAutomate } from '../../_lib/EntityAutomation';
 import { SignupLink } from '../../_lib/invitation/SignupLink';
 import { debugLog, error, errorResponse, invalidResponse, log, lookupCloudfrontDomain, okResponse } from "../../Utils";
 import { EntityToDemolish } from '../authorized-individual/Demolition';
-import { Task as ReAdminTasks, createEntity, createEntityAndInviteUsers, deactivateEntity, inviteUser, inviteUsers, lookupEntity, updateEntity } from '../re-admin/ReAdminUser';
+import { Task as ReAdminTasks, createEntity, createEntityAndInviteUsers, deactivateEntity, inviteUser, inviteUsers, lookupEntity, updateEntity, retractInvitation } from '../re-admin/ReAdminUser';
 import { DynamoDbTableOutput } from './DynamoDbTableOutput';
 import { HtmlTableView } from './view/HtmlTableView';
 
@@ -78,6 +78,8 @@ export const handler = async (event:any):Promise<LambdaProxyIntegrationResponse>
           return await inviteUsers(parameters);
         case ReAdminTasks.CREATE_ENTITY_INVITE:
           return await createEntityAndInviteUsers(parameters);
+        case ReAdminTasks.RETRACT_INVITATION:
+          return await retractInvitation(parameters.code);
         case ReAdminTasks.PING:
           return okResponse('Ping!', parameters);
         case Task.REPLACE_RE_ADMIN:
