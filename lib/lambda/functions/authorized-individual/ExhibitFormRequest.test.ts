4083d6e3f0476d4a2aa14694dcaf5e40f19d628f
Hennemuth <wrh@bu.edu>
2025-01-29 12:50:17 -0500

    BUGFIX: The exhibit form request email link was not properly formed

---
+++ lib/lambda/functions/authorized-individual/ExhibitFormRequest.test.ts
@@ -0,0 +1,95 @@
+import { SendExhibitFormRequestParms } from "./AuthorizedIndividual";
+import { ExhibitFormRequest } from "./ExhibitFormRequest";
+
+const expectedLink = (expected:string) => {
+  return { test: (parms:SendExhibitFormRequestParms) => {
+    const efr = new ExhibitFormRequest(parms);
+    const link = efr.getLink() as string;
+    expect(link).toEqual(expected);
+  }};
+}
+
+const defaultParms = {
+  consenterEmail: 'cp1@warhen.work',
+  entity_id: 'ea14dcfd-2f5a-40e1-9bc1-48a3afeec996',
+  linkUri: 'https://d227na12o3l3dd.cloudfront.net',
+  constraint: 'other',
+} as SendExhibitFormRequestParms;
+
+
+describe('ExhibitFormRequest', () => {
+
+  it('Should properly form an exhibit form request link for bootstrap exhibit form "backdoor"', async () => {
+    const parms = { ...defaultParms, linkUri: `${defaultParms.linkUri}/bootstrap/index.htm` };
+    let expected = 'https://d227na12o3l3dd.cloudfront.net/bootstrap/consenting/add-exhibit-form/other/index.htm';
+
+    // Test without trailing slash
+    parms.linkUri = `https://d227na12o3l3dd.cloudfront.net/bootstrap/index.htm`;
+    expectedLink(expected).test(parms);
+
+    // Test with trailing slash
+    parms.linkUri = `https://d227na12o3l3dd.cloudfront.net/bootstrap/index.htm/`;
+    expectedLink(expected).test(parms);
+
+    // Test without index.htm and trailing slash
+    parms.linkUri = `https://d227na12o3l3dd.cloudfront.net/bootstrap`;
+    expectedLink(expected).test(parms);
+
+    // Test without index.htm 
+    parms.linkUri = `https://d227na12o3l3dd.cloudfront.net/bootstrap/`;
+    expectedLink(expected).test(parms);
+    
+    // Test with a querystring
+    parms.linkUri = `https://d227na12o3l3dd.cloudfront.net/bootstrap/index.htm?foo=bar`;
+    expectedLink(`${expected}?foo=bar`).test(parms);
+    
+    // Test with a querystring and a trailing slash
+    parms.linkUri = `https://d227na12o3l3dd.cloudfront.net/bootstrap/index.htm/?foo=bar`;
+    expectedLink(`${expected}?foo=bar`).test(parms);
+    
+    // Test with a querystring and no file name
+    parms.linkUri = `https://d227na12o3l3dd.cloudfront.net/bootstrap?foo=bar`;
+    expectedLink(`${expected}?foo=bar`).test(parms);
+    
+    // Test with a querystring and no file name and a trailing slash
+    parms.linkUri = `https://d227na12o3l3dd.cloudfront.net/bootstrap/?foo=bar`;
+    expectedLink(`${expected}?foo=bar`).test(parms);
+  });
+
+  it('Should properly form an exhibit form request link for cloudfront for the standard website exhibit form "backdoor"', async () => {
+    const parms = { ...defaultParms };
+    const expected = 'https://d227na12o3l3dd.cloudfront.net/consenting/add-exhibit-form/other';
+
+    // Test without trailing slash
+    parms.linkUri = `https://d227na12o3l3dd.cloudfront.net`;
+    expectedLink(expected).test(parms);
+
+    // Test with trailing slash
+    parms.linkUri = `https://d227na12o3l3dd.cloudfront.net/`;
+    expectedLink(expected).test(parms);
+
+    // Test with index.html
+    parms.linkUri = `https://d227na12o3l3dd.cloudfront.net/index.html`;
+    expectedLink(expected).test(parms);
+
+    // Test with index.html and slash
+    parms.linkUri = `https://d227na12o3l3dd.cloudfront.net/index.html/`;
+    expectedLink(expected).test(parms);
+    
+    // Test with a querystring and no file name
+    parms.linkUri = `https://d227na12o3l3dd.cloudfront.net?foo=bar`;
+    expectedLink(`${expected}?foo=bar`).test(parms);
+    
+    // Test with a querystring and no file name and a trailing slash
+    parms.linkUri = `https://d227na12o3l3dd.cloudfront.net/?foo=bar`;
+    expectedLink(`${expected}?foo=bar`).test(parms);
+    
+    // Test with a querystring and a file name
+    parms.linkUri = `https://d227na12o3l3dd.cloudfront.net/index.html?foo=bar`;
+    expectedLink(`${expected}?foo=bar`).test(parms);
+    
+    // Test with a querystring and a file name and a trailing slash
+    parms.linkUri = `https://d227na12o3l3dd.cloudfront.net/index.html/?foo=bar`;
+    expectedLink(`${expected}?foo=bar`).test(parms);
+  });
+});
---
+++ lib/lambda/functions/authorized-individual/ExhibitFormRequest.ts
@@ -0,0 +1,115 @@
+import { LambdaProxyIntegrationResponse } from "../../../role/AbstractRole";
+import { errorResponse, invalidResponse, okResponse } from "../../Utils";
+import { SendExhibitFormRequestParms } from "./AuthorizedIndividual";
+import { ExhibitFormRequestEmail } from "./ExhibitFormRequestEmail";
+
+export class ExhibitFormRequest {
+  private parms: SendExhibitFormRequestParms;
+
+  constructor(parms:SendExhibitFormRequestParms) {
+    this.parms = parms;
+  }
+
+  /**
+   * If no link is provided, attempt to construct it from what can be found in environment variables.
+   * @returns 
+   */
+  private getDefaultLinkUri = ():string|void => {
+    const cloudfrontDomain = process.env.CLOUDFRONT_DOMAIN;
+    if( ! cloudfrontDomain) return;
+    return `https://${process.env.CLOUDFRONT_DOMAIN}`
+  }
+
+  /**
+   * Test to see if the basic href indicates the bootstrap website - a truthy return value means it does.
+   * @param url 
+   * @returns 
+   */
+  private tryAsBootstrapLink = (url:URL):string|void => {
+    const { parms: { constraint } } = this;
+    const { pathname:path } = url;
+
+    // Bail out if the url does not indicate a bootstrap link
+    const isABootstrapLink = () => path.endsWith('/bootstrap') || path.startsWith('/bootstrap/')
+    if( ! isABootstrapLink()) return;
+
+    let pathParts = path.split('/');
+    // Strip out index.htm if it is there. It should be at the end, not the middle, but account for that anyway
+    pathParts = pathParts.filter(part => part && part != 'index.htm');
+
+    url.pathname = `${pathParts.join('/')}/consenting/add-exhibit-form/${constraint}/index.htm`;
+    return url.href;
+  }
+
+  /**
+   * Test to see if the basic href indicates the standard website - a truthy return value means it does.
+   * @param url 
+   * @returns 
+   */
+  private tryAsStandardWebsiteLink = (url:URL):string|void => {
+    const { parms: { constraint } } = this;
+    const { pathname:path } = url;
+
+    let pathParts = path.split('/');
+    // Strip out any html file if it is there. It should be at the end, not the middle, but account for that anyway
+    pathParts = pathParts.filter(part => part && /^\w+\.((htm)|(html))$/i.test(part) == false);
+
+    url.pathname = `${pathParts.join('/')}/consenting/add-exhibit-form/${constraint}`;
+    return url.href;
+  }
+
+  /**
+   * Get the href of the link to be sent in the exhibit form request email.
+   * @returns 
+   */
+  public getLink = (): string|void => {
+    let { parms: { linkUri }, getDefaultLinkUri, tryAsBootstrapLink, tryAsStandardWebsiteLink } = this;
+    const href = linkUri ?? getDefaultLinkUri();
+    if( ! href) return;
+    const url = new URL(href);
+
+    let link = tryAsBootstrapLink(url);
+    if(link) return link;
+
+    // Test to see if the basic href indicates the standard website - a truthy return value means it does.
+    link = tryAsStandardWebsiteLink(url);
+    if(link) return link;
+
+    // There was something wrong with the basic href;
+    return;
+  }
+
+  /**
+   * Send the exhibit form request email
+   * @returns 
+   */
+  public sendEmail = async ():Promise<LambdaProxyIntegrationResponse> => {
+    let { parms: { consenterEmail, constraint, entity_id }, getLink } = this;
+
+    const linkUri = getLink();
+  
+    if( ! linkUri) {
+      return errorResponse('Email failure for exhibit form request: Cannot determine link to put in the email!');
+    }
+  
+    switch(constraint) {
+      case 'current': case 'other': case 'both':
+        const sent = await new ExhibitFormRequestEmail({ 
+          consenterEmail, 
+          entity_id, 
+          linkUri, 
+          constraint 
+        }).send();
+  
+        // Bail out if the email failed
+        if( ! sent) {
+          return errorResponse(`Email failure for exhibit form request: ${JSON.stringify({ consenterEmail, entity_id }, null, 2)}`);
+        }
+        
+        return okResponse('Ok', {});
+        
+      default:
+        return invalidResponse(`Invalid/missing affiliate constraint parameter: ${constraint}`);
+    }
+  }
+}
